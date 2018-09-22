6131466341397a6e980c13272ba92729e973ebd1
hamid moghadam <h.rayatmoghadam@gmail.com>
2018-09-22 09:24:42 +0330

    add migration of the previous commit

---
+++ UIMS.Web/Migrations/20180922055406_makeDayInPresentationNullable.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UIMS.Web.Migrations
+{
+    public partial class makeDayInPresentationNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Day",
+                table: "Presentation",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Day",
+                table: "Presentation",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+        }
+    }
+}
